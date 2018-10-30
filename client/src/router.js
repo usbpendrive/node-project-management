d0539368412118fedf8d7ef0f161be711675c1e7
Fernando <ferry.fernando@gmail.com>
2018-10-30 15:11:24 +0700

    Client side registration.

--- client/src/router.js
+++ client/src/router.js
@@ -4,22 +4,27 @@ import Home from './views/Home.vue'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: Home,
+      meta: { title: 'Enamel' }
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/signup/:id',
+      name: 'signup',
+      component: Signup,
+      meta: { title: 'Signup - enamel' }
     }
   ]
 })
+
+router.afterEach((to, from) => {
+  document.title = to.meta.title
+})
+
+export default router
