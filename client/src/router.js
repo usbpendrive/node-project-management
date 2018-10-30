6fdd6b2e59e42ab4fe5e9fae59c167e1fb30d811
Fernando <ferry.fernando@gmail.com>
2018-10-30 15:34:48 +0700

    Login form.

--- client/src/router.js
+++ client/src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Signup from './views/Signup'
+import Login from './views/Login'
 
 Vue.use(Router)
 
@@ -20,6 +21,12 @@ const router = new Router({
       name: 'signup',
       component: Signup,
       meta: { title: 'Signup - enamel' }
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login,
+      meta: { title: 'Login - enamel' }
     }
   ]
 })
